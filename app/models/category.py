284cd0429debcfdd6b4f2c21b6f911c6a1a18f28
Guilherme Libardi <guilhermelibardi@hotmail.com>
2024-06-05 23:37:29 -0300

    fix: made category name unique

--- app/models/category.py
+++ app/models/category.py
@@ -2,6 +2,6 @@ from ..extensions import db
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
     products = db.relationship('Product', back_populates='category', secondary='product_category')
     
